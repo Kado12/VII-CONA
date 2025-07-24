e0b08784af6788e08df73b73f655516fab923684
Kado12 <gonzalosh01@gmail.com>
2025-07-24 14:54:59 -0500

    update registration dates in Inscriptions and Schedule components

--- client/src/pages/Schedule.jsx
+++ client/src/pages/Schedule.jsx
@@ -19,11 +19,11 @@ const createData = (time, activity) => {
 const rows = [
   createData('Lanzamiento público del VII CONA, página web y facebook', '17 de enero 2025'),
   createData('Inicio de preinscripciones e inicio de Convocatoria para ponentes', '04 de marzo 2025'),
-  createData('Cierre de envío de resúmenes de trabajos', '18 de julio 2025'),
-  createData('Cierre de preinscripciones', '30 de julio 2025'),
-  createData('Inicio de Inscripciones', '01 de agosto 2025'),
-  createData('Publicación del Programa oficial', '07 de agosto 2025'),
-  createData('Cierre del proceso de revisión y confirmación de ponentes', '30 de agosto 2025'),
+  createData('Cierre de envío de resúmenes de trabajos', '30 de agosto 2025'),
+  createData('Cierre de preinscripciones', '30 de agosto 2025'),
+  createData('Inicio de Inscripciones', '01 de setiembre 2025'),
+  createData('Publicación del Programa oficial', '20 de setiembre 2025'),
+  createData('Cierre del proceso de revisión y confirmación de ponentes', '30 de setiembre 2025'),
   createData('Cursos-talleres precongreso', '04 de noviembre 2025'),
   createData('Congreso', '04 al 08 de noviembre 2025'),
   createData('Publicación y exposición de resultados ', '15 de diciembre 2025')
